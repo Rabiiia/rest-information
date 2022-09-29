6f02822c40f02465de6215fd373c716033bfdce1
acmve <acmverden@hotmail.com>
2022-09-29 11:44:52 +0200

    entities done

---
+++ src/main/java/entities/Phone.java
@@ -0,0 +1,47 @@
+package entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "phone")
+public class Phone {
+    @Id
+    @Column(name = "number", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "person_id", nullable = false)
+    private Person person;
+
+    @Size(max = 45)
+    @Column(name = "description", length = 45)
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
