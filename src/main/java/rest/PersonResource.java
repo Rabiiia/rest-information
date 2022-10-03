0e8d0a4d83f85ecb870c5f1a2220be0da56b404d
Mathias Fantoni <MathiasFantoni@Gmail.com>
2022-10-03 04:40:55 +0200

    Reintroduction of resource

---
+++ src/main/java/rest/PersonResource.java
@@ -0,0 +1,19 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import facades.PersonFacade;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("person")
+public class PersonResource {
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+    private static final PersonFacade FACADE = PersonFacade.getInstance(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+}
