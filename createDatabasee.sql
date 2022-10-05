2df8cb0afdd0414a84d81376acbaa14c5e7bd45d
Rabiiia <74189957+Rabiiia@users.noreply.github.com>
2022-10-05 15:27:53 +0200

    Update and rename createTables.sql to createDatabasee.sql

--- createDatabasee.sql
+++ createDatabasee.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE `information` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
 CREATE TABLE `address` (
   `address_id` int NOT NULL AUTO_INCREMENT,
   `street` varchar(45) NOT NULL,
